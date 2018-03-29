f988a230a4f8015c4338868d3b748b7761d2198e
ARSALAN KHAN <S338968746@ocdsb>
2018-03-29 09:32:43 -0400

    not done yet

--- main.py
+++ main.py
@@ -3,22 +3,28 @@
 # <Arsalan. K>
 # March 28, 2018
 
+
 M = str(input('Converting from Celcius or Fahrenheit: '))
 if M == 'Celcius':
-    while C > -273.15:
-        print CtoF ()
+    print (CtoF ())
 else:
-    while F > -459.67:
-        print FtoC ()
+    print (FtoC ())
 
 
 def CtoF ():
     C = int(input('Enter your temperature in Celsius: '))
-    F = (1.8)*C+32
-    return round(F)
-    #int(input('The entered value is invalid. Please enter a different value:'
+    while C > -273.15:
+        F = (1.8)*C+32
+        return round(F)
+        break
+    else:
+        C = int(input('The entered value is invalid. Please enter a different value: ')
 
 def FtoC ():
     F = int(input('Enter your temperature in Fahrenheit: '))
-    C = (0.55556)*(F-32)
-    return round(C)
+    while F > -459.67:
+        C = (0.55556)*(F-32)
+        return round(C)
+        break
+    else:
+        F = int(input('The entered value is invalid. Please enter a different value: ')
